e07827c2e19e43d20b1cf286b967b9c569a874ee
argraur <argraur@gmail.com>
2018-11-01 18:15:08 +0000

    dipper: AOSiP-Ify
    Signed-off-by: argraur <argraur@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := aosip_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 8
